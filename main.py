5fb2d6c5ced47c53b4de018f004b7905eb608e27
TeeToo <91502673+TeeToo136999@users.noreply.github.com>
2022-03-23 09:49:09 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+import os
+import time
+
+
+list = []
+list2 = []
+
+
+def main():
+    team = 0
+    player = int(input("zadej pocet hracu: "))
+    if player % 2 == 0 and player <= 10:
+        x = input("zadej lidi: ")
+        list = x.split()
+        while team < player / 2:
+            team1 = random.choice(list)
+            list2.append(team1)
+            list.remove(team1)
+            team += 1
+
+        print(f"\nTeam 1 je: {list2}")
+        print(f"\nTeam 2 je: {list}")
+        map()
+    else:
+        print("Spatny pocet hracu")
+        time.sleep(3)
+        os.system("cls")
+        main()
+
+
+def map():
+    map_random = ["Bind", "Haven", "Split", "Ascent", "Icebox", "Breeze"]
+    print(f"\nmapa je: {random.choice(map_random)}")
+    time.sleep(90)
+
+
+if __name__ == "__main__":
+    main()
